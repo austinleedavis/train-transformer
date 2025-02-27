c955994bead9e8c40957594892c281f0cd2433c5
Austin Davis <austindavis@gmail.com>
2025-02-27 09:54:11 -0500

    Add context manager to handle notifications and WandB integration in training loop

--- src/train.py
+++ src/train.py
@@ -1,11 +1,14 @@
 import os
+from contextlib import contextmanager
 
 import hydra
 import pyrootutils
 import torch
+from hydra.core.hydra_config import HydraConfig
 from lightning.pytorch import Trainer
 from omegaconf import DictConfig
 
+import wandb
 from data_module import LichessDataModule
 from gpt_module import GPT2Lightning
 from ntfy import Ntfy
@@ -32,6 +35,7 @@ def main(config: DictConfig):
     print(config)
     with Ntfy(topic=os.environ.get("NTFY_TOPIC", None)).context("Job"):
 
+    with handle_ntfy_wandb(config):
         dm = LichessDataModule(config)
         model = GPT2Lightning(config)
 
@@ -44,6 +48,34 @@ def main(config: DictConfig):
         model.hparams.lr = new_lr
 
         trainer.fit(model=model, datamodule=dm)
+
+
+@contextmanager
+def handle_ntfy_wandb(config: DictConfig):
+    ntfy = Ntfy(topic=os.environ.get("NTFY_TOPIC", None))
+    try:
+        hc = HydraConfig.get()
+        project = hc.job.name
+        name = "/".join(hc.run.dir.split("/")[1:])
+        extra_headers = {"Title": f"{project} {name}"}
+        if any(["WandbLogger" in list(cb.values())[0] for cb in config.trainer.logger]):
+            run = wandb.init(project=project, name=name, config=OmegaConf.to_object(config))
+            url = run.get_url()
+            extra_headers["Click"] = url
+
+        ntfy.send_notification(f"🤖Started", extra_headers=extra_headers)
+        final_text = f"🏆️ Finished"
+        exit_code = 0
+
+        yield
+    except Exception as e:
+        final_text = f"💢 Exception occurred {e}"
+        exit_code = -1
+        raise
+    finally:
+        ntfy.send_notification(final_text, extra_headers=extra_headers)
+        if run:
+            run.finish(exit_code)
 
 
 if __name__ == "__main__":
