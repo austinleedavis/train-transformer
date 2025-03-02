5011d4cd3711236f9f110a8317feeaccd86100a4
Austin Davis <austindavis@gmail.com>
2025-03-02 14:27:32 -0500

    add fix for llm_dtype mapping

--- src/gpt_module.py
+++ src/gpt_module.py
@@ -39,6 +39,9 @@ class GPT2Lightning(L.LightningModule):
         }
         llm_dtype = llm.config.torch_dtype
 
+        if llm_dtype in dtype_mapping.keys():
+            llm_dtype = dtype_mapping[forced_dtype]
+
         if forced_dtype := self.cfg.run.get("force_dtype", None):
             llm_dtype = dtype_mapping[forced_dtype]
 
